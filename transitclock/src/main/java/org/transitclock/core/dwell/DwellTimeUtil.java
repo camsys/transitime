2fe9169d176afd7d36d2e24e29ed5091cfebbbba
rjasmin-camsys <rjasmin@camsys.com>
2023-11-17 10:48:01 -0500

    Adding in comments for dwell algorithm.

--- transitclock/src/main/java/org/transitclock/core/dwell/DwellTimeUtil.java
+++ transitclock/src/main/java/org/transitclock/core/dwell/DwellTimeUtil.java
@@ -12,6 +12,8 @@ public class DwellTimeUtil {
 
     /**
      * Specify minimum allowable time in msec when calculating dwell time for departures.
+     * Currently hard coded to be 1 second. Should we allow this to be configurable?
+     * Store preferences in the DB?
      * @return
      */
     private static long getMinAllowableDwellTime() {
@@ -25,6 +27,8 @@ public class DwellTimeUtil {
 
     /**
      * Specifying the Max allowable time when calculating dwell time for departures.
+     * Currently hard coded to 1 minute. Should we allow this to be configurable?
+     * Store preferences in the DB?
      * @return
      */
     private static long getMaxAllowableDwellTime() {
@@ -40,14 +44,17 @@ public class DwellTimeUtil {
     public static Long getDwellTime(Long arrivalTime, Long departureTime,Block block,int tripIndex,
                               int departureStopPathIndex, Integer arrivalStopPathIndex){
 
+        //Logic for when it is the first stop
         if (departureStopPathIndex == 0 && departureTime != null) {
             Trip trip = block.getTrip(tripIndex);
             if (trip != null) {
                 return calculateFirstStopDwellTime(trip.getStartTime(), departureTime);
             }
+        //Logic for all other stops except for the last stop (no dwell for last stop)
         } else if (departureStopPathIndex != block.numStopPaths(tripIndex) - 1) {
             if (arrivalTime != null && departureTime != null &&
                     arrivalStopPathIndex != null && departureStopPathIndex == arrivalStopPathIndex) {
+                //get the dwell time by doing some simple subtraction :)
                 return recalculateDwellTimeUsingThresholds(departureTime - arrivalTime);
             }
         } else {
@@ -61,18 +68,24 @@ public class DwellTimeUtil {
             long tripStartTimeMsecs = scheduledStartTime * 1000;
             Time time = Core.getInstance().getTime();
             long msecsIntoDay = time.getMsecsIntoDay(new Date(departureTime), tripStartTimeMsecs);
+            // Dwell for 0 seconds if the departure time is earlier than the trip start time
+            // could be an issue in the data given or the bus could be late
             if (msecsIntoDay < tripStartTimeMsecs) {
                 return 0l;
             } else {
+            //get the dwell time by doing some simple subtraction :)
                 return recalculateDwellTimeUsingThresholds(msecsIntoDay - tripStartTimeMsecs);
             }
     }
 
     private static Long recalculateDwellTimeUsingThresholds(Long dwellTime){
+        //Should we return the min dwell time if the dwell time is null?
         if(dwellTime != null){
             if(dwellTime < getMinAllowableDwellTime()){
                 return getMinAllowableDwellTime();
             }
+            //If dwell time is less than max, return dwell time.
+            //Should we return max time if the dwell time is over max?
             else if(dwellTime <= getMaxAllowableDwellTime()){
                 return dwellTime;
             }
