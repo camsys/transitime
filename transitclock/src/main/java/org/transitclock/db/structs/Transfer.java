9dfbcee28ea43d04f63bb234e14e58d0612f0987
sheldonabrown <sheldonb@gmail.com>
2024-03-16 16:56:12 -0400

    TransferDAO refactor

--- transitclock/src/main/java/org/transitclock/db/structs/Transfer.java
+++ transitclock/src/main/java/org/transitclock/db/structs/Transfer.java
@@ -89,40 +89,6 @@ public class Transfer implements Serializable {
 		transferType = null;
 		minTransferTime = null;
 	}
-	
-	/**
-	 * Deletes rev 0 from the Transfers table
-	 * 
-	 * @param session
-	 * @param configRev
-	 * @return Number of rows deleted
-	 * @throws HibernateException
-	 */
-	public static int deleteFromRev(Session session, int configRev) 
-			throws HibernateException {
-		// Note that hql uses class name, not the table name
-		String hql = "DELETE Transfer WHERE configRev=" + configRev;
-		int numUpdates = session.createQuery(hql).executeUpdate();
-		return numUpdates;
-	}
-
-	/**
-	 * Returns List of Transfer objects for the specified database revision.
-	 * 
-	 * @param session
-	 * @param configRev
-	 * @return
-	 * @throws HibernateException
-	 */
-	@SuppressWarnings("unchecked")
-	public static List<Transfer> getTransfers(Session session, int configRev) 
-			throws HibernateException {
-		String hql = "FROM Transfer " +
-				"    WHERE configRev = :configRev";
-		Query query = session.createQuery(hql);
-		query.setInteger("configRev", configRev);
-		return query.list();
-	}
 
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
